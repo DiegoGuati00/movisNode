09ed523c349732b0190740e047f99a32ae5c10e9
Diego <diego@MacBook-Pro-de-Diego.local>
2022-03-28 20:20:41 -0500

    #

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { app } = require('./app');
 const { config } = require('./config/config');
+const { initModel } = require('./utils/initModels');
 
 // Utils
 const { sequelize } = require('./utils/database');
@@ -8,7 +9,7 @@ sequelize
     .authenticate()
     .then(() => console.log('Database authenticated'))
     .catch((err) => console.log(err));
-
+initModel();
 const PORT = config.DBport || 4000;
 
 app.listen(PORT, () => {
